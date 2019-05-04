970b35a201132eae82e725a3a8d2ee9a3ce35151
AshleyF <ashley.feniello@live.com>
2019-05-03 18:47:56 -0700

    Initial (partially functional) Brief -> WebAssembly

---
+++ Utility.fs
@@ -0,0 +1,9 @@
+﻿module Utility
+
+open System
+open System.IO
+
+let save (wasm: byte seq) =
+    let base64 = wasm |> Array.ofSeq |> Convert.ToBase64String
+    use writer = File.CreateText("./main.js")
+    writer.WriteLine(sprintf "run('%s');" base64)
